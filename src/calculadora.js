c020f1b44de11d774d74473a45c80560f0f3cd80
Mateus <davi santos>
2025-07-01 19:47:13 -0300

    Operações Matematicas

---
+++ src/calculadora.js
@@ -0,0 +1,23 @@
+const soma = (a,b) => {
+    res = a + b;
+    return(res) 
+}
+
+const subtrcao = (a,b) => {
+    res = a - b;
+    return(res)
+}
+
+const multiplicacao = (a,b) => {
+    res = a * b;
+    return(res);
+}
+
+const divisao = () => {
+    if(b != 0){
+    res = a / b;
+        return(res)
+    } else {
+        console.error("O denominador não pode ser 0 vagabundo")
+    }
+}
